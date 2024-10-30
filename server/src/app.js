ae027dfa2553d704f3e955676f6c56e959d85ae7
sr-roboto <marcoslopez123.lu@gmail.com>
2024-10-30 14:15:33 -0300

    Agregar páginas de inicio y login, configurar Axios, y añadir soporte para Tailwind CSS

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import { applyMiddlewares } from './middlewares/app.middleware.js';
+import { userAuthRouter } from './routes/userAuth.route.js';
 
 // Inicializar la aplicación
 const app = express();
 
 // Aplicar middlewares
 applyMiddlewares(app);
+
+// Rutas
+app.use('/auth', userAuthRouter);
 
 export { app };
--- server/src/middlewares/app.middleware.js
+++ server/src/middlewares/app.middleware.js
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 const applyMiddlewares = (app) => {
   app.use(
     cors({
-      origin: 'http://localhost:3000',
+      origin: 'http://localhost:5173',
       credentials: true,
     })
   );
